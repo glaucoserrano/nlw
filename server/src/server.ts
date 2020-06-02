e1b3fca25b31e2a206b0fe79cf8c75ff94e9ac3e
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-06-02 13:02:27 -0300

    Segundo dia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request,response) => {
-   console.log('Listagem de usuarios');
+app.use(express.json());
+app.use(routes);
 
-   response.json([
-      "glauco",
-      "marcelo",
-      "thaty",
-      "waldeir",
-      "suzete"
-   ]);
-});
+app.use("/uploads",express.static(path.resolve(__dirname,'..','uploads')));
+
 
 app.listen(3333);
